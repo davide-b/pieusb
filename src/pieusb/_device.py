cb51910c22974996b31c64d38d98bb38ebbf646a
Davide Bongiovanni <davidebongiov@gmail.com>
2026-07-22 15:49:06 +0200

    Imported USB information

--- src/pieusb/_device.py
+++ src/pieusb/_device.py
@@ -1,1 +1,11 @@
 PIE_VENDOR_ID = 0x05E3
+KNWON_PIDS = [0x0142, 0x0144, 0x0145]
+MODEL_NAMES = {
+    0x003a: 'DigitDia 6000 Multiple Slide Scanner',
+    0x0030: 'CrystalScan 7200',
+    0x0036: 'ProScan 7200',
+    0x0047: 'ProScan 10T',
+    0x002e: 'CrystalScan 3600',
+    0x002f: 'DigitDia 4000',
+    0x0048: 'RPS 10M (aka Pacific Image PrimeFilm XAs)',
+}
